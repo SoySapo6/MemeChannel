75ffb1d91f0a6f7db83f01928178983b67a5d780
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-31 20:16:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express()
 
-app.use(express.static(__dirname)) // sirve los .ts y .m3u8
+app.use(express.static(__dirname)) // sirve .ts, .mp4 y demás archivos estáticos
 
-app.get('/', (req, res) => {
-  res.send(`<video src="/canal.m3u8" controls autoplay></video>`)
+app.get('/canal.m3u8', (req, res) => {
+  res.type('application/vnd.apple.mpegurl');
+  res.sendFile(path.join(__dirname, 'canal.m3u8'));
 })
 
 app.listen(process.env.PORT || 3000, () => {
